378df62ea9bf3d453aa37f0d1a86086f1be97c47
student <student@umcs.pl>
2023-03-08 14:37:32 +0100

    oop

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Locale;
+
+public class Main {
+
+    public static final Point test = new Point(2,3);
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Polygon.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+
+public class Polygon {
+    private Point[] arr;
+    private Style styleSettings;
+
+    public Polygon(int count, Style styleSettings) {
+        arr = new Point[count];
+        this.styleSettings = styleSettings;
+    }
+
+    public void setPoint(int index, Point point) {
+        arr[index] = point;
+    }
+
+    public void setPoints(Point[] points){
+        arr = points;
+    }
+
+    public String toSvg() {
+        String pointsString = "";
+        for(Point point : arr)
+            pointsString += point.x + "," + point.y + " ";
+
+        return String.format(Locale.ENGLISH,"<polygonn points=\"%s\" style=\"%s\"/>", pointsString, styleSettings.toSvg());
+    }
+
+
+}
